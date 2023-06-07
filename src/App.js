ac269012c579246465d03972f0af0970f993a71f
ZhannaZucher <jzucher@gmail.com>
2023-06-07 18:04:24 +0200

    fix : style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, BrowserRouter } from "react-router-dom"
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Accommodation from "./pages/Accommodation/Accommodation"
 import Error from './pages/Error/Error'
-import './style/index.css'
+//import './style/index.css'
 
 function App() {
   return (
-    <div id="wrapper">
+    <BrowserRouter>
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
@@ -18,8 +18,7 @@ function App() {
         <Route path="*" element={<Error/>} />
       </Routes>
       <Footer/>
-    </div>
-
+    </BrowserRouter>
   )
 }
 
