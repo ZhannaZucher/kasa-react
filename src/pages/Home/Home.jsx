e0445d4763bf9d9806aae0f200cde238397bab1b
ZhannaZucher <jzucher@gmail.com>
2023-06-08 20:41:45 +0200

    add: content render

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,32 @@
+import dataHousing from "../../data/housing.json"
+import { Link } from "react-router-dom"
 import Banner from "../../components/Banner/Banner"
 import BannerImg from "../../assets/img-home.jpg"
+import Card from "../../components/Card/Card"
 
 
 
 export default function Home() {
 	return (
-		<Banner home="true">
-			<img className="banner__img" src={BannerImg} alt="rocks" />
-			<h1 className="banner__title">Chez vous, partout et ailleurs</h1>
-		</Banner>
+		<main>
+			<Banner home="true">
+				<img className="banner__img" src={BannerImg} alt="rocks" />
+				<h1 className="banner__title">Chez vous, partout et ailleurs</h1>
+			</Banner>
+			<section className="housing-content">
+				{
+					dataHousing.map((house) => (
+						<Link key={house.id} to={`/accommodation/${house.id}`}>
+							<Card 
+								label={house.title}
+								picture={house.cover}
+							/>
+						</Link>
+					))
+				}
+			</section>
+		</main>
+
 	)
 }
 
