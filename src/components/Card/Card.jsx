24a35ce55c33e3bc2ebd9d8859c9ba8ddf11fa24
ZhannaZucher <jzucher@gmail.com>
2023-06-08 20:41:32 +0200

    add : card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import "./Card.css"
+
+export default function Card ( {label, picture} ) {
+	return (
+		<article className="accommodation-card">
+			<div className="accommodation-card__view">
+				<img src={picture} alt="logement"/>
+			</div>
+			<h2 className="accommodation-card__label">{label}</h2>
+		</article>
+	)
+}
+
+Card.propTypes = {
+	label: PropTypes.string,
+	picture: PropTypes.string,
+}
