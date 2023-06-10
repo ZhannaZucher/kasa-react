8d2213a6673eed70b827fb131a9ea8a7824407bc
ZhannaZucher <jzucher@gmail.com>
2023-06-10 11:18:14 +0200

    add: Link to logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
 import Logo from "../../assets/logo.svg"
 import "./Header.css"
 
 export default function Header() {
 	return (
 		<header className="header">
-			<div className="header__logo">
+			<Link className="header__logo" to="/">
 				<img src={Logo} alt="logo" />
-			</div>
+			</Link>
 			<nav className="header__nav">
 				<NavLink to="/" className={({ isActive }) => isActive ? "active" : undefined}>Accueil</NavLink>
 				<NavLink to="/about" className={({ isActive }) => isActive ? "active" : undefined}>A Propos</NavLink>
