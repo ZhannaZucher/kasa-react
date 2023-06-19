dd2deca0619146c037eaf5cf90fba414bb63e4db
ZhannaZucher <jzucher@gmail.com>
2023-06-19 17:49:45 +0200

    restore routes

--- src/App.js
+++ src/App.js
@@ -6,24 +6,14 @@ import About from "./pages/About/About"
 import Accommodation from "./pages/Accommodation/Accommodation"
 import Error from "./pages/Error/Error"
 
-let baseURL = ""
-if (process.env.NODE_ENV === "production") {
-  baseURL = "Kasa/"
-} else {
-  baseURL = "/"
-}
-
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route exact path={baseURL + ""} element={<Home />} />
-        <Route path={baseURL + "about"} element={<About />} />
-        <Route
-          path={baseURL + "accommodation/:id"}
-          element={<Accommodation />}
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/accommodation/:id" element={<Accommodation />} />
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
